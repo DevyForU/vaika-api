72931f4ac3746c64059892f10c063987de54d144
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-11 15:11:24 +0300

    feat:add appointment as model

---
+++ src/models/appointment.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Appointment extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  message: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  contact: string;
+
+  @property({
+    type: 'date',
+    default: now(),
+  })
+  appointmentDate?: string;
+
+  @property({
+    type: 'string',
+    default: unknow,
+  })
+  status?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Appointment>) {
+    super(data);
+  }
+}
+
+export interface AppointmentRelations {
+  // describe navigational properties here
+}
+
+export type AppointmentWithRelations = Appointment & AppointmentRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './user.model';
 export * from './car.model';
 export * from './image.model';
+export * from './appointment.model';
